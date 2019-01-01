23a7502ac2ba26916cc6984355804f7fecd5ad41
Ankiii001 <ankithbti177@gmail.com>
2019-01-02 01:41:25 +0530

    Styling Components with ngStyle

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -7,7 +7,7 @@ import { Component, Input } from '@angular/core';
     <h2>{{event?.name}}</h2>
     <!--<img [src]="event?.imageUrl"/>-->
     <div>Date: {{event?.date}}</div>
-    <div [ngClass]="getStartTimeClass()" [ngSwitch]="event?.time">
+    <div [ngStyle]="getStartTimeStyle()" [ngSwitch]="event?.time">
         Time: {{event?.time}}
         <span *ngSwitchCase="'8:00 am'">Early Start</span>
         <span *ngSwitchCase="'10:00 am'">Late Start</span>
@@ -23,8 +23,6 @@ import { Component, Input } from '@angular/core';
 `,
 // adding styles to components
 styles: [`
-    .green { color: #003300 !important; }
-    .bold { font-weight: bold;}
     .thumbnail { min-height: 210px;}
     .pad-left { margin-left: 10px; }
     .well div { color: #bbb; }
@@ -34,10 +32,10 @@ styles: [`
 export class EventThumbnailComponent {
     @Input() event: any;
 
-    getStartTimeClass(){
+    getStartTimeStyle(): any {
         if (this.event && this.event.time === '8:00 am') {
-            return ['green', 'bold'];
+            return {color: '#003300', 'font-weight': 'bold'};
         }
-            return [];
+            return {};
     }
 }
