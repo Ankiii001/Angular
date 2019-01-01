bc3f071da0b71cf4257cc254834864a0b8f10d1a
Ankiii001 <ankithbti177@gmail.com>
2019-01-02 01:16:11 +0530

    Styling Components with Class

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -7,8 +7,8 @@ import { Component, Input } from '@angular/core';
     <h2>{{event?.name}}</h2>
     <!--<img [src]="event?.imageUrl"/>-->
     <div>Date: {{event?.date}}</div>
-    <div>Time: {{event?.time}}</div>
-    <div [ngSwitch]="event?.time">
+    <div [class.green]="event?.time === '8:00 am'" [ngSwitch]="event?.time">
+        Time: {{event?.time}}
         <span *ngSwitchCase="'8:00 am'">Early Start</span>
         <span *ngSwitchCase="'10:00 am'">Late Start</span>
         <span *ngSwitchDefault>Normal Start</span>
@@ -23,6 +23,7 @@ import { Component, Input } from '@angular/core';
 `,
 // adding styles to components
 styles: [`
+    .green { color: #003300 !important; }
     .thumbnail { min-height: 210px;}
     .pad-left { margin-left: 10px; }
     .well div { color: #bbb; }
