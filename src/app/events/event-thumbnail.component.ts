f7612d123920796076fa4c70bb47712552f886cb
Ankiii001 <ankithbti177@gmail.com>
2019-01-02 01:22:05 +0530

    multiple Styling with ngClass

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -7,7 +7,7 @@ import { Component, Input } from '@angular/core';
     <h2>{{event?.name}}</h2>
     <!--<img [src]="event?.imageUrl"/>-->
     <div>Date: {{event?.date}}</div>
-    <div [class.green]="event?.time === '8:00 am'" [ngSwitch]="event?.time">
+    <div [ngClass]="{green: event?.time === '8:00 am', bold: event?.time === '10:00 am'}" [ngSwitch]="event?.time">
         Time: {{event?.time}}
         <span *ngSwitchCase="'8:00 am'">Early Start</span>
         <span *ngSwitchCase="'10:00 am'">Late Start</span>
@@ -24,6 +24,7 @@ import { Component, Input } from '@angular/core';
 // adding styles to components
 styles: [`
     .green { color: #003300 !important; }
+    .bold { font-weight: bold;}
     .thumbnail { min-height: 210px;}
     .pad-left { margin-left: 10px; }
     .well div { color: #bbb; }
