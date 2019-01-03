143580a08b1d052c39de65521142065a9635084a
Ankiii001 <ankithbti177@gmail.com>
2019-01-04 01:40:46 +0530

    lazily loading feature modules

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -13,8 +13,9 @@ export const appRoutes: Routes = [
         canDeactivate: ['canDeactivateCreateEvent']},
     { path: 'events', component: EventsListComponent, resolve:
         { events: EventListResolver }},
-    { path: 'events/:id', component: EventDetailsComponent, 
+    { path: 'events/:id', component: EventDetailsComponent,
         canActivate: [EventRouteActivator]},
+        { path: 'user', loadChildren: './user/user.module#UserModule' },
     { path: '404', component: Error404Component},
     { path: '**', redirectTo: '/events', pathMatch: 'full'}
 ];
